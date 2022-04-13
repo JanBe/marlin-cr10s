570fd0e6fa0fa865f684c4241f79523d296d0214
Jan Bechstein <jan@bechstein.me>
2022-04-13 22:10:03 +0200

    Reduce homing speed for x and y by 50%

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1756,7 +1756,7 @@
 #endif
 
 // Homing speeds (mm/min)
-#define HOMING_FEEDRATE_MM_M { (50*60), (50*60), (8*60) }
+#define HOMING_FEEDRATE_MM_M { (25*60), (25*60), (8*60) }
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
